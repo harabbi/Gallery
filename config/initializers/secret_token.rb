54977154cdeb7ae8abeb764d9e9af85de76f3408
root <root@webhost.sonsofruthie.net>
2012-09-12 22:20:42 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = 'eab5390b1565674de187c03675a28318eea3a815d24888171b971e085d16d6ac2fdb3fc36256025a2b7a61782d43658e491b6acaaae2b34093ad6eed2c17cbfc'
